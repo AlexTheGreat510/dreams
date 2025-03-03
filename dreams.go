e84efa1bb4fed9c2abd58e7347825606b3868fd7
AlexTheGreat510 <alexthegrea600@gmail.com>
2025-03-03 16:43:33 -0500

    added: wait-type flag, some other fixes

--- dreams.go
+++ dreams.go
@@ -1,5 +1,7 @@
 package main
 
+/* import modules */
+
 import (
     "fmt"
     "flag"
@@ -15,18 +17,38 @@ func main() {
 
     wait := flag.Int("wait", 1, "Set the execution time.")
     command := flag.String("command", "random-wallpaper", "Set the command.")
+    waitType := flag.String("wait-type", "minutes", "Set the wait type [seconds, minutes, hours].")
     
     /* parse flags */
-    
+
     flag.Parse()
     
-    /* start the infinite loop */
+    /* check the time type */
     
+    var timeType time.Duration
+
+    switch *waitType {
+
+        case "seconds":
+            timeType = time.Second
+
+        case "minutes":
+            timeType = time.Minute
+
+        case "hours":
+            timeType = time.Hour
+
+        default:
+            timeType = time.Minute
+    }
+
+    /* start the infinite loop */
+
     for true {
-        
+
         /* show the message if running command */
 
-        fmt.Println("dreams: Running command", *command)
+        fmt.Printf("dreams: Running command '%s'\n", *command)
 
         /* let program execute the program */
 
@@ -39,6 +61,6 @@ func main() {
         
         /* let program sleep */
 
-        time.Sleep(time.Duration(*wait) * time.Minute)
+        time.Sleep(time.Duration(*wait) * timeType)
     }
 }
